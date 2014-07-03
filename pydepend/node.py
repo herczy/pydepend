2aeee56c943c5ac2e8127b1fff9e7a8caf5f444f
Hercinger Viktor <hercinger.viktor@balabit.com>
2014-07-03 11:25:55 +0200

    node: added parent and root property to node

--- pydepend/node.py
+++ pydepend/node.py
@@ -2,17 +2,29 @@ import ast
 
 
 class Node(object):
-    def __init__(self, ast):
+    def __init__(self, ast, parent=None):
         self.__ast = ast
+        self.__parent = parent
 
     @property
     def ast(self):
         return self.__ast
 
+    @property
+    def parent(self):
+        return self.__parent
+
+    @property
+    def root(self):
+        if self.__parent is not None:
+            return self.__parent.root
+
+        return self
+
 
 class TerminalNode(Node):
-    def __init__(self, name, ast, filename):
-        super(TerminalNode, self).__init__(ast)
+    def __init__(self, name, ast, filename, parent=None):
+        super(TerminalNode, self).__init__(ast, parent=parent)
 
         self.__name = name
         self.__filename = filename
--- pydepend/tests/test_node.py
+++ pydepend/tests/test_node.py
@@ -7,10 +7,20 @@ from ..node import Node, TerminalNode
 class TestNode(unittest.TestCase):
     def setUp(self):
         self.ast = ast.parse('ast').body[0]
-        self.node = Node(self.ast)
+        self.parent = Node(None)
+        self.node = Node(self.ast, parent=self.parent)
 
     def test_ast(self):
         self.assertEqual(self.ast, self.node.ast)
+
+    def test_parent(self):
+        self.assertEqual(self.parent, self.node.parent)
+
+    def test_root(self):
+        self.assertEqual(self.parent, self.node.root)
+
+    def test_root_of_root(self):
+        self.assertEqual(self.parent, self.parent.root)
 
 
 class TestTerminalNode(unittest.TestCase):
