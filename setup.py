a4a35d2fda7c994d6ae69e636ee1c601c31e2d17
Hercinger Viktor <hercinger.viktor@balabit.com>
2014-07-27 10:29:40 +0200

    added minimal setup file

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os.path
+import sys
+import glob
+
+try:
+    from setuptools import setup
+
+except ImportError:
+    from distutils.core import setup
+
+
+setup(name="pydepend",
+      description="JDepend clone for Python 2 and 3",
+      license="BSD",
+      version='0.1',
+      author="Viktor Hercinger",
+      author_email="hercinger.viktor@gmail.com",
+      maintainer="Viktor Hercinger",
+      maintainer_email="hercinger.viktor@gmail.com",
+      packages=[
+        'pydepend',
+        'pydepend.metric',
+        'pydepend.report',
+      ])
